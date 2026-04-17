0a58f72c783842942217d470dea84664619a9295
Adam Essmyer <adamessmyer@Adams-MacBook-Pro.local>
2026-04-17 07:43:24 -0500

    added floating numbers and numeric operations

--- main.rs
+++ main.rs
@@ -6,4 +6,26 @@ fn main() {
     println!("The value of x in the inner scope is: {x}");
    }
    println!("The value of x is: {x}");
-}
+
+   // Floating point numbers
+
+      let _a = 2.0;
+
+      let _b: f32 = 3.0;
+
+
+      //numeric operations
+      let sum = 5 + 10;
+
+      let difference = 24.5 - 4.3;
+
+      let product = 6 * 7;
+
+      let quotient = 56.7 / 32.2;
+
+      let truncated = -5 / 3;
+
+      let remainder = 43 % 5;
+   }
+
+   
