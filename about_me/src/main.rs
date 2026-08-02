85707e97b64b51984d29aebf5999feba40375448
Adam Essmyer <adamessmyer@MacBookPro.lan>
2026-08-02 14:54:30 -0500

    modified about me section, ran cargo fmt

--- about_me/src/main.rs
+++ about_me/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("I am 37 years old. \nI want to be a software engineer. \nI am like coffee.");
+    println!("I am 37 years old. \nI want to be a software engineer. \nI like coffee.");
 }
