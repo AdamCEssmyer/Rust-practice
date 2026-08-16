cc58f29aab5a02a72c981a35291798d1e01e1ef7
Adam Essmyer <adamessmyer@MacBookPro.lan>
2026-08-16 15:32:15 -0500

    alias code practice in main.rs

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,13 +1,18 @@
 
 const TAX_RATE: f64 = 0.08;
+type Meters = i32;
 fn main() {
     //scope
     let coffee_price: f64 = 5.99;
     //constant
+
+    //alias
+    let mile_race_length: Meters = 1600;
     println!("The tax rate is {TAX_RATE}");
 
     {
-       println!("The price is {coffee_price}"); 
+       println!("The price is {coffee_price}");
+       println!("A one mile race is {mile_race_length} meters") ;
     }
 }
 
